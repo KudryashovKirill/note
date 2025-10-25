13e28dc5c2d6db5e406c40855a2002c4cab44afe
Kirill <kkn-20052@yandex.ru>
2025-10-26 00:29:18 +0300

    added Dockerfile and docker-compose.yaml for work in docker. Tested in postman

--- src/main/java/com/example/note/demo/controller/TagController.java
+++ src/main/java/com/example/note/demo/controller/TagController.java
@@ -18,7 +18,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("/tag")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-@CrossOrigin(origins = {"http://127.0.0.1:5533", "http://localhost:5533", "http://127.0.0.1:5500", "http://localhost:5500"})
 public class TagController {
     TagService tagService;
 
